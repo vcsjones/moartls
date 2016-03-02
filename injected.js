84aa293b304a55458c556d4406e24cba458ace0a
Eric Lawrence <ericlaw1979@hotmail.com>
2016-03-01 22:58:50 -0600

    Use QuerySelectorAll to find links
    Faster, and avoid <A> that lack hrefs.

--- injected.js
+++ injected.js
@@ -5,21 +5,21 @@ if ((document.location.protocol == "http:") ||
     document.body.style.backgroundColor="#E04343";
 }
 
-    var lnks = document.getElementsByTagName("a");
-    var arrUnsecure = [];
-    for(var i = 0; i < lnks.length; i++) {
-        var thisLink = lnks[i];
-        var sProtocol = thisLink.protocol.toLowerCase();
-        if ((sProtocol == "http:") || (sProtocol == "ftp:")) {
-            arrUnsecure.push(thisLink.href);
-            thisLink.style.backgroundColor = "#DE6A6A";
-            thisLink.style.borderRadius = "4px";
-            thisLink.style.border = "2px solid red";
-            thisLink.style.padding = "6px 6px 6px 6px";
-            thisLink.style.margin = "3px 3px 3px 3px";
-            thisLink.title = lnks[i].protocol + "//" + lnks[i].hostname;
-        }
-    }
+var lnks = document.querySelectorAll("a[href]");
+var arrUnsecure = [];
+for (var i = 0; i < lnks.length; i++) {
+  var thisLink = lnks[i];
+  var sProtocol = thisLink.protocol.toLowerCase();
+  if ((sProtocol == "http:") || (sProtocol == "ftp:")) {
+    arrUnsecure.push(thisLink.href);
+    thisLink.style.backgroundColor = "#DE6A6A";
+    thisLink.style.borderRadius = "4px";
+    thisLink.style.border = "2px solid red";
+    thisLink.style.padding = "6px 6px 6px 6px";
+    thisLink.style.margin = "3px 3px 3px 3px";
+    thisLink.title = lnks[i].protocol + "//" + lnks[i].hostname;
+  }
+}
 
 //https://developer.chrome.com/extensions/messaging
 chrome.runtime.sendMessage({cLinks: lnks.length, unsecure: arrUnsecure },
