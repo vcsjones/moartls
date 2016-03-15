73ee6a75393b8bb2989d8736585745fd6884545e
Eric Lawrence <ericlaw1979@hotmail.com>
2016-03-15 15:53:00 -0500

    Use Strict

--- options.js
+++ options.js
@@ -1,3 +1,5 @@
+"use strict";
+
 document.addEventListener('DOMContentLoaded', function() {
 
     chrome.storage.sync.get(null, function(prefs) 
@@ -7,17 +9,17 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     var checkboxes = document.querySelectorAll("input[type=checkbox]");
-    for (i=0; i<checkboxes.length; i++) {
+    for (let i=0; i<checkboxes.length; i++) {
         checkboxes[i].addEventListener('change', saveChanges, false);
     }
 
 }, false);
 
 function saveChanges() {
-    var status = document.getElementById("txtStatus");
+    let status = document.getElementById("txtStatus");
     status.textContent = "Saving...";
-    var cbRotateImages = document.getElementById("cbRotateImages");
-    var cbWarnOnNonSecureDownloads = document.getElementById("cbWarnOnNonSecureDownloads");
+    let cbRotateImages = document.getElementById("cbRotateImages");
+    let cbWarnOnNonSecureDownloads = document.getElementById("cbWarnOnNonSecureDownloads");
     chrome.storage.sync.set({"bRotateNonSecureImages": cbRotateImages.checked, 
                              "bWarnOnNonSecureDownloads": cbWarnOnNonSecureDownloads.checked}, null);
 
